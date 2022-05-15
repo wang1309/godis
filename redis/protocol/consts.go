014c51bec01c67d3945a82983047fab30c907963
王瑞 <wangrui5@songguo7.com>
2022-05-15 16:55:38 +0800

    add parse

---
+++ redis/protocol/consts.go
@@ -0,0 +1,31 @@
+package protocol
+
+var nullBulkBytes = []byte("$-1\r\n")
+
+// NullBulkReply is empty string
+type NullBulkReply struct{}
+
+// ToBytes marshal redis.Reply
+func (r *NullBulkReply) ToBytes() []byte {
+	return nullBulkBytes
+}
+
+// MakeNullBulkReply creates a new NullBulkReply
+func MakeNullBulkReply() *NullBulkReply {
+	return &NullBulkReply{}
+}
+
+var emptyMultiBulkBytes = []byte("*0\r\n")
+
+// EmptyMultiBulkReply is a empty list
+type EmptyMultiBulkReply struct{}
+
+// ToBytes marshal redis.Reply
+func (r *EmptyMultiBulkReply) ToBytes() []byte {
+	return emptyMultiBulkBytes
+}
+
+// MakeEmptyMultiBulkReply creates EmptyMultiBulkReply
+func MakeEmptyMultiBulkReply() *EmptyMultiBulkReply {
+	return &EmptyMultiBulkReply{}
+}
